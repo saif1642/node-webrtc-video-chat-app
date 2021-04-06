887dff2073c295fade3dd7032597d4b47eee4ae7
saif1642 <islumsaiful@gmail.com>
2021-04-06 14:54:44 +0600

    receiver setup

--- sender/sender.js
+++ sender/sender.js
@@ -1,4 +1,4 @@
-const webSocket = new WebSocket("ws://SERVER-IP-HERE:3000")
+const webSocket = new WebSocket("")
 
 webSocket.onmessage = (event) => {
     handleSignallingData(JSON.parse(event.data))
